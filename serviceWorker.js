4c9610382d2723adcf972c72583b82d6a0ebcc40
jeff93063 <70969969+jeff93063@users.noreply.github.com>
2023-12-10 15:47:38 -0800

    Makes serviceWorker delete old cache when a new versionis added

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,29 +1,33 @@
 const staticCacheName = "poker-static-v1";
 const assets = [
-  "/poker/",
-  "/poker/index.html"
+	"/poker/",
+	"/poker/index.html"
 ];
 
 self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticCacheName).then(cache => {
-      cache.addAll(assets)
-    })
-  )
+	installEvent.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			cache.addAll(assets);
+		})
+	);
 });
 
 self.addEventListener('activate', evt => {
 	evt.waitUntil(
 		caches.keys().then(keys =>{
-			console.log(keys);
+			//console.log(keys);
+			return Promise.all(keys
+				.filter(key => key !== staticCacheName)
+				.map(key => caches.delete(key))
+			)
 		})
-	)
+	);
 });
 
 self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request);
+		})
+	);
 });
